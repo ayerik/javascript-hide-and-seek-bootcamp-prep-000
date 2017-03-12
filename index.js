e1f9e672fff578828b8b918206d88686e33427ae
Tracy Copeland <ayerik@hotmail.com>
2017-03-12 18:52:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var list = document.getElementById(`app').querySelectorAll('ul.ranked-list li');
+
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = (i + n).toString();
+  }
+
+  return list;
+}
