49e8e3ba092398d159b7246b378dd525f15866fc
Tracy Copeland <ayerik@hotmail.com>
2017-03-13 16:48:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var path = "#grande-node";
+
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
@@ -7,11 +9,21 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var list = document.getElementById(`app').querySelectorAll('ul.ranked-list li');
+  var list = document.getElementById("app").querySelectorAll("ul.ranked-list li");
 
   for (var i = 0; i < list.length; i++) {
-    list[i].innerHTML = (i + n).toString();
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString();
   }
 
   return list;
+}
+
+function deepestChild() {
+  if (document.querySelector(path) === null) {
+    return path;
+  } else {
+    path.append(document.querySelector(path));;
+    deepestChild();
+    return path;
+  }
 }
